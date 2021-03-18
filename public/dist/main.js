17b3b1eda8fa74ccca9ec2d8f70e18dde545b24f
Signe <signees96@gmail.com>
2021-03-18 11:08:07 +0100

    Revert "Fixed /service to set the actually servicename after slash and removed the servicename parameter in main.js"
    This reverts commit 8ae03518b1cbfac1e55889bb753c8d7b5bd2a084.

--- public/dist/main.js
+++ public/dist/main.js
@@ -16870,12 +16870,13 @@ exports.viskort = function(id,ticket,options) {
   var map = new L.Map(id, options);
 
   function danKort(service,layer,styles,transparent) {
-    return L.tileLayer.wms('https://api.dataforsyningen.dk/' + service, 
-        {
-            format: 'image/png',
-            maxZoom: 14,
-            minZoom: 2,
-            ticket: ticket,
+		return L.tileLayer.wms('https://api.dataforsyningen.dk/service', 
+			{
+				format: 'image/png',
+				maxZoom: 14,
+				minZoom: 2,
+				ticket: ticket,
+				servicename: service,
 	  		attribution: 'Data</a> fra <a href="https://api.dataforsyningen.dk">DAWA</a> | Map data &copy;  <a href="https://sdfe.dk">SDFE</a>',
 	  		layers: layer,
 	  		styles: styles,
